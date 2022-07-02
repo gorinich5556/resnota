3f73c0df2cdfc853260ab1d995ddd18a21248ed2
Egor Kovalov <101421082+gorinich5556@users.noreply.github.com>
2022-07-02 12:37:04 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -48,29 +48,7 @@ catalogButton.addEventListener('click', function(){
     }
 })
 
-// BUTTONS FOR MENU OF CATALOG
 
-const buttonMenuHits = document.querySelector('.product__menu-item_hits')
-const buttonMenuDiscounts = document.querySelector('.product__menu-item_discounts')
-const buttonMenuNovetlly = document.querySelector('.product__menu-item_novelties')
-
-// animated buttons
-
-buttonMenuHits.addEventListener('click', function(){
-    buttonMenuHits.classList.add('product__menu-item-active')
-    buttonMenuDiscounts.classList.remove('product__menu-item-active')
-    buttonMenuNovetlly.classList.remove('product__menu-item-active')
-})
-buttonMenuDiscounts.addEventListener('click', function(){
-    buttonMenuDiscounts.classList.add('product__menu-item-active')
-    buttonMenuHits.classList.remove('product__menu-item-active')
-    buttonMenuNovetlly.classList.remove('product__menu-item-active')
-})
-buttonMenuNovetlly.addEventListener('click', function(){
-    buttonMenuNovetlly.classList.add('product__menu-item-active')
-    buttonMenuDiscounts.classList.remove('product__menu-item-active')
-    buttonMenuHits.classList.remove('product__menu-item-active')
-})
 
 // SWIPER KATALOG
 
@@ -98,3 +76,89 @@ new Swiper('.product__items', {
       },
   });
 
+// FAVORITE
+
+const favoriteIcon = document.querySelectorAll('.icon-favorite')
+var favoriteIconState = false
+
+favoriteIcon.forEach(item => {
+    item.addEventListener('click', function(){
+        if (favoriteIconState == true){
+            item.classList.remove('icon-Star-2')
+            item.classList.add('icon-favorite')
+            favoriteIconState = false
+        }  else{
+            item.classList.add('icon-Star-2')
+            item.classList.remove('icon-favorite')
+            favoriteIconState = true
+        }
+    })
+})
+
+// CHANGE CATALOG SWIPER
+
+const SwiperHits = document.querySelector('.product__items_hits')
+const SwiperDiscount = document.querySelector('.product__items_discount')
+const SwiperNovetlly = document.querySelector('.product__items_novetlly')
+
+function changeCatalogSwiper(SwiperHits, productHitsState, SwiperDiscount, productDiscountState){
+    if (productDiscountState == true){
+        SwiperDiscount.style.display = 'block'
+        SwiperHits.style.display = 'none'
+        SwiperNovetlly.style.display = 'none'
+    }
+    if (productHitsState == true){
+        SwiperDiscount.style.display = 'none'
+        SwiperHits.style.display = 'block'
+        SwiperNovetlly.style.display = 'none'
+    }
+    if (productNovetllyState == true){
+        SwiperDiscount.style.display = 'none'
+        SwiperHits.style.display = 'none'
+        SwiperNovetlly.style.display = 'block'
+    }
+}
+changeCatalogSwiper(SwiperHits, productHitsState, SwiperDiscount, productDiscountState)
+// BUTTONS FOR MENU OF CATALOG
+
+const buttonMenuHits = document.querySelector('.product__menu-item_hits')
+const buttonMenuDiscounts = document.querySelector('.product__menu-item_discounts')
+const buttonMenuNovetlly = document.querySelector('.product__menu-item_novelties')
+
+var productDiscountState = true
+var productHitsState = false
+var productNovetllyState = false
+
+// animated default
+
+buttonMenuDiscounts.classList.add('product__menu-item-active')
+
+// animated onclick buttons
+
+buttonMenuHits.addEventListener('click', function(){
+    buttonMenuHits.classList.add('product__menu-item-active')
+    buttonMenuDiscounts.classList.remove('product__menu-item-active')
+    buttonMenuNovetlly.classList.remove('product__menu-item-active')
+    productHitsState = true
+    productDiscountState = false
+    productNovetllyState = false
+    changeCatalogSwiper(SwiperHits, productHitsState, SwiperDiscount, productDiscountState)
+})
+buttonMenuDiscounts.addEventListener('click', function(){
+    buttonMenuDiscounts.classList.add('product__menu-item-active')
+    buttonMenuHits.classList.remove('product__menu-item-active')
+    buttonMenuNovetlly.classList.remove('product__menu-item-active')
+    productHitsState = false
+    productDiscountState = true
+    productNovetllyState = false
+    changeCatalogSwiper(SwiperHits, productHitsState, SwiperDiscount, productDiscountState)
+})
+buttonMenuNovetlly.addEventListener('click', function(){
+    buttonMenuNovetlly.classList.add('product__menu-item-active')
+    buttonMenuDiscounts.classList.remove('product__menu-item-active')
+    buttonMenuHits.classList.remove('product__menu-item-active')
+    productHitsState = false
+    productDiscountState = false
+    productNovetllyState = true
+    changeCatalogSwiper(SwiperHits, productHitsState, SwiperDiscount, productDiscountState)
+})
